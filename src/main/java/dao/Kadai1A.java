217b8b5e5698f9b0e95917f058daaf7bea418b4d
masa <m.moriai.sys22@morijyobi.ac.jp>
2023-01-17 14:29:40 +0900

    ログイン機能の新規作成

---
+++ src/main/java/dao/Kadai1A.java
@@ -0,0 +1,117 @@
+package dao;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dto.Kadai1B;
+import util.GenerateHashedPw;
+import util.GenerateSalt;
+public class Kadai1A {
+	
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+	    String username = dbUri.getUserInfo().split(":")[0];
+	    String password = dbUri.getUserInfo().split(":")[1];
+	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+	    return DriverManager.getConnection(dbUrl, username, password);
+	}
+
+	public static int register(Kadai1B kadai1b) {
+		String sql = "INSERT INTO kadai1b VALUES(default, ?, ?, ?, ?, ?, ?, ?,current_timestamp)";
+		int result = 0;
+		
+		// ランダムなソルトの取得(今回は32桁で実装)
+		String salt = GenerateSalt.getSalt(32);
+		
+		// 取得したソルトを使って平文PWをハッシュ
+		String hashedPw = GenerateHashedPw.getSafetyPassword(kadai1b.getPassword(), salt);
+	
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			pstmt.setString(1, kadai1b.getName());
+			pstmt.setInt(2, kadai1b.getAge());
+			pstmt.setString(3, kadai1b.getGender());
+			pstmt.setString(4, kadai1b.getTel());
+			pstmt.setString(5, kadai1b.getMail());
+			pstmt.setString(6, hashedPw);
+			pstmt.setString(7, salt);
+
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println(result + "件更新しました。");
+		}
+		return result;
+	}
+	
+	// メールアドレスを元にソルトを取得
+		public static String getSalt(String mail) {
+			String sql = "SELECT salt FROM kadai1b WHERE mail = ?";
+			
+			try (
+					Connection con = getConnection();
+					PreparedStatement pstmt = con.prepareStatement(sql);
+					){
+				pstmt.setString(1, mail);
+
+				try (ResultSet rs = pstmt.executeQuery()){
+					
+					if(rs.next()) {
+						String salt = rs.getString("salt");
+						return salt;
+					}
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+	public static Kadai1B login(String mail, String hashedPw) {
+		String sql = "SELECT * FROM kadai1b WHERE mail = ? AND password = ?";
+		
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			pstmt.setString(1, mail);
+			pstmt.setString(2, hashedPw);
+
+			try (ResultSet rs = pstmt.executeQuery()){
+				
+				if(rs.next()) {
+					int id = rs.getInt("id");
+					String name = rs.getString("name");
+					String salt = rs.getString("salt");
+					String createdAt = rs.getString("created_at");
+					
+					return new Kadai1B(id,name, -1,null,null,mail,salt,null,null);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
